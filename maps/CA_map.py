fea93b05d2326cc0ed3d32026b8ee9d2b7002669
Liam Bindle <liambindle@gmail.com>
2020-07-07 23:41:59 -0600

    foo

--- maps/CA_map.py
+++ maps/CA_map.py
@@ -8,181 +8,6 @@ import os.path
 import ast
 import matplotlib.colors
 
-def get_minor_xy(xe, ye):
-    p0 = slice(0, -1)
-    p1 = slice(1, None)
-    boxes_x = np.moveaxis(np.array([xe[p0, p0], xe[p1, p0], xe[p1, p1], xe[p0, p1], xe[p0, p0]]), 0, -1)
-    boxes_y = np.moveaxis(np.array([ye[p0, p0], ye[p1, p0], ye[p1, p1], ye[p0, p1], ye[p0, p0]]), 0, -1)
-    return np.moveaxis(np.array([boxes_x, boxes_y]), 0, -1)
-
-
-def transform_xy(xy_in, in_proj: pyproj.Proj, out_proj: pyproj.Proj):
-    xy_out = pyproj.transform(in_proj, out_proj, xy_in[..., 0], xy_in[..., 1], always_xy=True)
-    xy_out = np.moveaxis(xy_out, 0, -1)
-    return xy_out
-
-
-def xy_to_polygons(xy, transform=None, error_on_bad_polygon=True, only_valid=False):
-    if len(xy.shape) == 2:
-        xy = np.expand_dims(xy, 0)
-
-    output_shape = xy.shape[:-2]
-
-    indexes = np.moveaxis(np.meshgrid(*[range(i) for i in xy.shape[:-2]], indexing='ij'), 0, -1)
-    stacked = np.product(xy.shape[:-2])
-    xy = np.reshape(xy, (stacked, *xy.shape[-2:]))
-    indexes = np.reshape(indexes, (stacked, len(xy.shape[-2:])))
-    polygons = []
-    bad = []
-    zero_area = []
-    index_lut = {}
-    for i, (polygon_xy, index) in enumerate(zip(xy, indexes)):
-        polygon = shapely.geometry.Polygon(polygon_xy)
-
-        is_bad = False
-        if np.count_nonzero(np.isnan(polygon_xy)) > 0:
-            bad.append(i)
-            is_bad = True
-        elif not polygon.is_valid:
-            bad.append(i)
-            is_bad = True
-        elif polygon.area <= 0:
-            zero_area.append(i)
-            is_bad = True
-
-        if not only_valid:
-            polygons.append(polygon)
-            index_lut[id(polygons[-1])] = tuple(index)
-        elif only_valid and not is_bad:
-            polygons.append(polygon)
-            index_lut[id(polygons[-1])] = tuple(index)
-
-    if error_on_bad_polygon and (len(bad) > 0 or len(zero_area) > 0):
-        raise RuntimeError('A bad polygon was detected')
-    elif not only_valid and (len(bad) > 0 or len(zero_area) > 0):
-        for bad_index in [*bad, *zero_area]:
-            polygons[bad_index] = shapely.geometry.Polygon([(0,0), (0,0), (0,0)]) # zero area
-
-    if only_valid:
-        return index_lut, polygons
-    else:
-        return np.reshape(polygons, output_shape)
-
-
-def determine_blocksize(xy, xc, yc):
-    latlon = pyproj.Proj('+init=epsg:4326')
-    N = xc.shape[0]
-    factors = [i for i in range(1, N+1) if N % i == 0]
-
-    for f in factors:
-        blocksize = N // f
-        nblocks = f
-
-        blocked_shape = (nblocks, nblocks, blocksize, blocksize)
-
-        try:
-            for bi in range(nblocks):
-                for bj in range(nblocks):
-
-                    block = np.ix_(range(bi*blocksize, (bi+1)*blocksize), range(bj*blocksize, (bj+1)*blocksize))
-
-                    xc_block = xc[block]
-                    yc_block = yc[block]
-                    xy_block = xy[block]
-
-                    center_x = xc_block[blocksize//2, blocksize//2]
-                    center_y = yc_block[blocksize//2, blocksize//2]
-
-                    local_gno = pyproj.Proj(ccrs.Gnomonic(center_y, center_x).proj4_init)
-
-                    block_xy_gno = transform_xy(xy_block, latlon, local_gno)
-                    _ = xy_to_polygons(block_xy_gno, error_on_bad_polygon=True)
-            return blocksize
-        except RuntimeError:
-            pass
-    raise RuntimeError('Failed to determine the appropriate blocksize')
-
-
-def get_am_and_pm_masks_and_polygons_outline(xe, ye, far_from_pm=80):
-    if np.any(xe >= 180):
-        raise ValueError('xe must be in [-180, 180)')
-    # xe must be [-180 to 180]
-    p0 = slice(0, -1)
-    p1 = slice(1, None)
-
-    # Mask where bounding box crosses the prime meridian or antimeridian
-    cross_pm_or_am_line1 = np.not_equal(np.sign(xe[p0, p0]), np.sign(xe[p1, p0]))
-    cross_pm_or_am_line2 = np.not_equal(np.sign(xe[p1, p0]), np.sign(xe[p1, p1]))
-    cross_pm_or_am_line3 = np.not_equal(np.sign(xe[p1, p1]), np.sign(xe[p0, p1]))
-    cross_pm_or_am_line4 = np.not_equal(np.sign(xe[p0, p1]), np.sign(xe[p0, p0]))
-    cross_pm_or_am = cross_pm_or_am_line1 | cross_pm_or_am_line2 | cross_pm_or_am_line3 | cross_pm_or_am_line4
-
-    # Make xy polygons for each gridbox
-    boxes_x = np.moveaxis(np.array([xe[p0, p0], xe[p1, p0], xe[p1, p1], xe[p0, p1]]), 0, -1)
-    boxes_y = np.moveaxis(np.array([ye[p0, p0], ye[p1, p0], ye[p1, p1], ye[p0, p1]]), 0, -1)
-    polygon_outlines = np.moveaxis(np.array([boxes_x, boxes_y]), 0, -1)
-
-    pm = np.ones((xe.shape[0]-1, xe.shape[1]-1), dtype=bool)
-    am = np.ones((xe.shape[0]-1, xe.shape[1]-1), dtype=bool)
-
-    # Figure out which polygon_outlines cross the prime meridian and antimeridian
-    crossing_indexes = np.argwhere(cross_pm_or_am)
-    for idx in crossing_indexes:
-        box = shapely.geometry.LinearRing(polygon_outlines[tuple(idx)])
-        far_from_the_prime_meridian = shapely.geometry.LineString([(far_from_pm, -90), (80, far_from_pm)])
-        if box.crosses(far_from_the_prime_meridian):
-            am[tuple(idx)] = False
-        else:
-            pm[tuple(idx)] = False
-
-    return am, pm, polygon_outlines
-
-
-def _pcolormesh2_internal(ax, X, Y, C, cmap, norm):
-    X[X >= 180] -= 360
-
-    am, pm, boxes_xy_pc = get_am_and_pm_masks_and_polygons_outline(X, Y)
-
-    center_i = int(X.shape[0] / 2)
-    center_j = int(X.shape[1] / 2)
-    cX = X[center_i, center_j]
-    cY = Y[center_i, center_j]
-
-    gnomonic_crs = ccrs.Gnomonic(cY, cX)
-    #gnomonic_proj = pyproj.Proj(gnomonic_crs.proj4_init)
-    gnomonic_proj = pyproj.Transformer.from_crs('epsg:4326', gnomonic_crs.proj4_init, always_xy=True).transform
-
-    X_gno, Y_gno = gnomonic_proj(X, Y)
-    boxes_xy_gno = np.moveaxis(gnomonic_proj(boxes_xy_pc[..., 0], boxes_xy_pc[..., 1]), 0, -1)
-
-    if np.any(np.isnan(X_gno)) or np.any(np.isnan(Y_gno)):
-        raise ValueError('Block size is too big!')
-    else:
-        plt.pcolormesh(X_gno, Y_gno, np.ma.masked_array(C, ~am), transform=gnomonic_crs, cmap=cmap, norm=norm)
-
-        for idx in np.argwhere(~am):
-            c = cmap(norm(C[idx[0], idx[1]]))
-            ax.add_geometries(
-                [shapely.geometry.Polygon(boxes_xy_gno[idx[0], idx[1],...])],
-                gnomonic_crs, edgecolor=c, facecolor=c
-            )
-
-
-def pcolormesh2(X, Y, C, blocksize, norm, **kwargs):
-    kwargs.setdefault('cmap', 'viridis')
-    cmap = plt.get_cmap(kwargs['cmap'])
-
-    ax = plt.gca()
-
-    for si, ei in [(s * blocksize, (s + 1) * blocksize + 1) for s in range(X.shape[0] // blocksize)]:
-        for sj, ej in [(s * blocksize, (s + 1) * blocksize + 1) for s in range(X.shape[1] // blocksize)]:
-            _pcolormesh2_internal(ax,
-                                  X[si:ei, sj:ej],
-                                  Y[si:ei, sj:ej],
-                                  C[si:ei - 1, sj:ej - 1],
-                                  cmap, norm
-                                  )
-
 
 if __name__ == '__main__':
     import xarray as xr
@@ -379,16 +204,15 @@ if __name__ == '__main__':
         exit(0)
 
     if 'nf' in da.dims:
-        # -128 -65 23 50
         for nf in range(6):
-            xe = grid.xe.isel(nf=nf).values % 360
-            ye = grid.ye.isel(nf=nf).values
+            # -128 -65 23 50
+            xe = grid['xe'].isel(nf=nf).values % 360
+            ye = grid['ye'].isel(nf=nf).values
             xc = grid['grid_boxes_centers'].isel(nf=nf, XY=0).values % 360
+            xc[xc > 180] -= 360
             yc = grid['grid_boxes_centers'].isel(nf=nf, XY=1).values
-            face_xy = get_minor_xy(xe % 360, ye)
-            blocksize = determine_blocksize(face_xy, xc % 360, yc)
-            # print(f'Block size for face {nf+1}: {blocksize}')
-            pcolormesh2(xe, ye, da.isel(nf=nf), blocksize, norm, cmap=cmap)
+            mask = (xc > -128) & (xc < -65) & (yc > 23) & (yc < 50)
+            plt.pcolormesh(xe, ye, np.ma.masked_array(da.isel(nf=nf).values, ~mask), transform=ccrs.PlateCarree(), cmap=cmap, norm=norm)
 
     else:
         xe, ye = grid.xe.values, grid.ye.values #pyproj.Transformer.from_crs('epsg:4326', 'epsg:2163', always_xy=True).transform(grid.xe, grid.ye)
